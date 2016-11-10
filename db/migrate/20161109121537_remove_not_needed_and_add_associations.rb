07554224bfde088412f3853211cf557c48024a2d
ttrinks <ttrinks.git@icloud.com>
2016-11-10 12:12:13 +0100

    removed migrate file

--- db/migrate/20161109121537_remove_not_needed_and_add_associations.rb
+++
@@ -1,6 +0,0 @@
-class RemoveNotNeededAndAddAssociations < ActiveRecord::Migration[5.0]
-  def self.up
-    remove_column :bankaccounts, :client_id
-    remove_column :bankaccounts, :bank_id
-  end
-end
